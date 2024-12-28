ebd2db22b9c94dddd868af11ee379778e9940bb8
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2024-12-28 21:31:46 +0200

    Taking out 2 comments
    Taking out 2 comments about 2 old line codes

--- kaunisvaasa.js
+++ kaunisvaasa.js
@@ -1,10 +1,6 @@
 console.log("Kaunis Vaasa - Finland");
 
-//let katsoKuva = "0Kuva";
-
 let pic = document.getElementById("kuva0");
-
-//let kuvat = ["img_0", "img_"]
 
 let kuvat = [];
 for (let index = 0; index <21; index++) {
