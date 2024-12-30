6b942b287cbef8bc50bafdd6361a464cd452d8b7
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2024-12-30 06:33:46 +0200

    Correcciones y add play sound
    Se quitan funciones que no estaban habilitadas
    Add function play para añadir efecto de sonido cuando suceda un match de las 3 pics.

--- pelikone.js
+++ pelikone.js
@@ -2,7 +2,7 @@ window.onload=inicio;
 
 // Se usa el array kuvat para las imagenes
 
-let premios =["amor", "salud", "dinero", "amistad", "viajes"]; // Mensajes numerados deacuerdo a la numeración de las pics
+let premios =["amor", "salud", "dinero", "amistad", "viajes"]; // Mensajes deacuerdo a la numeración de las pics
 
 let numeros_actuales=[];
 
@@ -26,14 +26,13 @@ function lanzar_inicio(){
 }
 
 function escoger_numero(){
-    let azar=Math.floor(Math.random() * 3);  //kuvat.lenght=5
+    let azar=Math.floor(Math.random() * 3);  //kuvat.lenght=3
     return azar;
 }
 
 function mostrar_imagen(num,im){
 
     document.getElementsByClassName("imagen")[num].getElementsByTagName("img")[0].src=kuvat[im];
-    
 }
 
 function comparar(){
@@ -41,24 +40,16 @@ function comparar(){
    const mensaje = document.getElementById("mensaje");
 
    if(numeros_actuales[0]==numeros_actuales[1] && numeros_actuales[1]==numeros_actuales[2]){
-        //let nuevoMensaje = premios[numeros_actuales[0]];
         
         mensaje.innerText = premios[numeros_actuales[0]];
-        console.log(numeros_actuales[0]);
+        console.log("Mensaje ganador: "+numeros_actuales[0]);
+
+       play();  // activa el audio
    }
 
 }
 
-function actualizar(){
-
-}
-
-
-
-function cerrar(){
-
-}
-
-function sonar(){
-
+function play(){
+    let audio = new Audio("http://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3");
+    audio.play();  
 }
