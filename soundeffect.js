471496d3545f6414d447f7ba309b87b0a291aacc
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2025-01-02 00:13:08 +0200

    Adding file "soundeffect.js"
    The sounds effects functions are gathered in one file to be called in other files

---
+++ soundeffect.js
@@ -0,0 +1,16 @@
+// window.onload = aloittaa;
+
+function soundVoittaja(){
+    let audioVoittaja = new Audio("https://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3");
+    audioVoittaja.play();     
+}
+
+function painaNappiSound(){
+    let audioNappiSound = new Audio("https://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3");
+    audioNappiSound.play();   
+}
+
+function muutaKuvaSound(){
+    let audioMuutaKuvaSound = new Audio("https://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3");
+    audioMuutaKuvaSound.play();   
+}
